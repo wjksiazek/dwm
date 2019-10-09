0e450bd14b2950a08a91af5a1a8034e4ad6fd318
WK <wjksiazek@gmail.com>
2019-10-09 16:18:45 +0200

    tags

--- config.def.h
+++ config.def.h
@@ -46,12 +46,12 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+ 	{ "[\\]",      dwindle },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "HHH",      grid },
  	{ "[@]",      spiral },
- 	{ "[\\]",      dwindle },
 	{ NULL,       NULL },
 };
 
@@ -71,7 +71,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", "-e tmuxdd",  NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34",  NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
